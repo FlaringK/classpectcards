3f013fbb6599c3f877f78e849bed4fb2f0121b41
FlaringK <caelans@iinet.net.au>
2023-01-19 21:37:22 +0800

    first commit

---
+++ draw.js
@@ -0,0 +1,190 @@
+// Canvas
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let getCardNumber = c => {
+  switch (c) {
+    case 0:
+      return "A"
+      break;
+    case 9:
+      return "J"
+      break;
+    case 10:
+      return "Q"
+      break;
+    case 11:
+      return "K"
+      break;
+  
+    default:
+      return c + 1
+      break;
+  }
+}
+
+let drawNumber = (c, a) => {
+  // Determine card number
+  let cardNumber = getCardNumber(c)
+  // Draw number
+  ctx.fillText(cardNumber, 42.5 * numberScale + numberX, 20 * numberScale + numberY)
+
+  // Draw symbol background
+  ctx.beginPath();
+  ctx.arc(45 * numberScale + numberX, 125 * numberScale + numberY, 30 * numberScale, 0, 2 * Math.PI);
+  ctx.fill();
+
+  // Draw symbol
+  ctx.filter = 'blur(0.5px)';
+  ctx.drawImage(aspectImgs[a], 20 * numberScale + numberX, 100 * numberScale + numberY, 50 * numberScale, 50 * numberScale)
+  ctx.filter = 'blur(0)';
+}
+
+let setCardSize = () => {
+  canvas.style = ""
+  canvas.width = cardSizeX
+  canvas.height = cardSizeY
+  setDPI(canvas, cardDPI)
+}
+
+// Actually draw card
+let drawCard = (c, a) => {
+
+  setCardSize()
+
+  // Clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw Class
+  ctx.drawImage(classImgs[c], (canvas.width - classSize) / 2, (canvas.height - classSize) / 2, classSize, classSize)
+  
+  // Fill Class with colour
+  ctx.globalCompositeOperation = 'source-in'
+  ctx.fillStyle = aspColors[a]
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  ctx.globalCompositeOperation = 'source-over'
+
+  // Draw Aspect 
+  ctx.drawImage(aspectImgs[a], (canvas.width - aspectSize) / 2, (canvas.height - aspectSize) / 2, aspectSize, aspectSize)
+
+  // DRAWING TEXT 
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+
+  // Draw Card Name
+  if (doDrawName) {
+    ctx.font = `${nameSize}px cardc`
+    ctx.strokeStyle = 'white'
+    ctx.lineWidth = nameStroke
+    const cardName = doNameAspect ? classes[c] + " of " + aspects[a] : "the " + classes[c]
+    const ypos = c == 5 || c == 11 ? nameY * -1 : nameY
+    ctx.strokeText(cardName, canvas.width / 2, canvas.height / 2 + ypos)
+    ctx.fillText(cardName, canvas.width / 2, canvas.height / 2 + ypos)
+  }
+
+  // Draw Top Number
+  ctx.textBaseline = 'top';
+  ctx.font = `${70 * numberScale}px cardc`
+  drawNumber(c, a)
+
+  // Draw Bottom Number
+  ctx.save()
+  ctx.translate( canvas.width, canvas.height )
+  ctx.rotate(Math.PI)
+  drawNumber(c, a)
+  ctx.restore()
+
+  // Fill background with white
+  ctx.globalCompositeOperation = 'destination-over'
+  ctx.fillStyle = "white"
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  ctx.globalCompositeOperation = 'source-over'
+}
+
+// View card 
+let viewCard = () => {
+  getOptions()
+  drawCard(viewClass, viewAspect)
+}
+
+// Update card when settings change
+document.querySelectorAll("#options input, #options select").forEach(e => {
+  e.onchange = viewCard
+})
+
+// https://stackoverflow.com/questions/14488849/higher-dpi-graphics-with-html5-canvas
+function setDPI(canvas, dpi) {
+  // Set up CSS size.
+  canvas.style.width = canvas.style.width || canvas.width + 'px';
+  canvas.style.height = canvas.style.height || canvas.height + 'px';
+
+  // Get size information.
+  var scaleFactor = dpi / 96;
+  var width = parseFloat(canvas.style.width);
+  var height = parseFloat(canvas.style.height);
+
+  // Backup the canvas contents.
+  var oldScale = canvas.width / width;
+  var backupScale = scaleFactor / oldScale;
+  var backup = canvas.cloneNode(false);
+  backup.getContext('2d').drawImage(canvas, 0, 0);
+
+  // Resize the canvas.
+  var ctx = canvas.getContext('2d');
+  canvas.width = Math.ceil(width * scaleFactor);
+  canvas.height = Math.ceil(height * scaleFactor);
+
+  // Redraw the canvas image and scale future draws.
+  ctx.save()
+  ctx.setTransform(backupScale, 0, 0, backupScale, 0, 0);
+  ctx.drawImage(backup, 0, 0);
+  ctx.setTransform(scaleFactor, 0, 0, scaleFactor, 0, 0);
+  ctx.restore()
+}
+
+// https://stackoverflow.com/questions/18480474/how-to-save-an-image-from-canvas
+function download(canvas, filename) {
+
+	/// create an "off-screen" anchor tag
+	var lnk = document.createElement('a'), e;
+
+	/// the key here is to set the download attribute of the a tag
+	lnk.download = filename;
+
+	/// convert canvas content to data-uri for link. When download
+	/// attribute is set the content pointed to by link will be
+	/// pushed as "download" in HTML5 capable browsers
+	lnk.href = canvas.toDataURL();
+
+	/// create a "fake" click-event to trigger the download
+  e = document.createEvent("MouseEvents");
+  e.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+
+  lnk.dispatchEvent(e);
+}
+
+// Download Viewable Card
+let downloadView = () => {
+  download(canvas, aspects[viewAspect] + getCardNumber(viewClass) + ".png")
+}
+
+let downloadSet = () => {
+  let zip = new JSZip();
+
+  aspects.forEach((a, i) => {
+
+    classes.forEach((c, j) => {
+
+      drawCard(j, i)
+      const imgUrl = canvas.toDataURL()
+      zip.file(a + "/" + a + getCardNumber(j) + ".png", imgUrl.split('base64,')[1], {base64: true})
+
+    })
+  })
+
+  zip.generateAsync({type:"blob"}).then(function(content) {
+    // see FileSaver.js
+    saveAs(content, "classpectCards.zip");
+  });
+
+}
---
+++ options.js
@@ -0,0 +1,85 @@
+// ===
+
+let cardSizeX = 559
+let cardSizeY = 871
+let cardDPI = 300
+
+let classSize = 1800
+let aspectSize = 600
+
+let numberScale = 3
+let numberX = 200
+let numberY = 200
+
+let doDrawName = true 
+let doNameAspect = true
+let nameY = 0
+let nameSize = 120
+let nameStroke = 20
+
+let viewAspect = 0
+let viewClass = 0
+
+// ===
+
+// Classes
+const classes = ["heir", "witch", "seer", "mage", "knight", "page", "rogue", "thief", "maid", "bard", "sylph", "prince"]
+const classImgs = []
+
+// Aspects
+const aspects = ["breath", "blood", "time", "space", "light", "void", "mind", "heart", "life", "doom", "hope", "rage"]
+const aspColors = ["#4379e6", "#3e1601", "#b70d0e", "#000000", "#f0840c", "#033476", "#00923d", "#55142a", "#ccc3b4", "#173220", "#ffe094", "#520c61"]
+const aspectImgs = []
+
+// Classes
+classes.forEach((className, i) => {
+  // Load Images
+  const img = new Image(500, 500)
+  img.src = `./classes/${className}.png`
+  classImgs.push(img)
+
+  document.getElementById("hidden").appendChild(img)
+  // Add view options
+  const option = document.createElement("option")
+  option.innerText = className
+  option.value = i
+  document.getElementById("viewClass").appendChild(option)
+})
+
+// Aspects
+aspects.forEach((aspectName, i) => {
+  const img = new Image()
+  img.src = `./aspects/${aspectName}.png`
+  aspectImgs.push(img)
+
+  document.getElementById("hidden").appendChild(img)
+  // Add view options
+  const option = document.createElement("option")
+  option.innerText = aspectName
+  option.value = i
+  document.getElementById("viewAspect").appendChild(option)
+})
+
+// Get options
+
+let getOptions = () => {
+  cardSizeX = document.getElementById("cardSizeX").value
+  cardSizeY = document.getElementById("cardSizeY").value
+  cardDPI = document.getElementById("cardDPI").value
+
+  classSize = document.getElementById("classSize").value
+  aspectSize = document.getElementById("aspectSize").value
+
+  numberScale = parseFloat(document.getElementById("numberScale").value)
+  numberX = parseFloat(document.getElementById("numberX").value)
+  numberY = parseFloat(document.getElementById("numberY").value)
+
+  doDrawName = document.getElementById("doDrawName").checked
+  doNameAspect = document.getElementById("doNameAspect").checked
+  nameY = parseFloat(document.getElementById("nameY").value)
+  nameSize = parseFloat(document.getElementById("nameSize").value)
+  nameStroke = parseFloat(document.getElementById("nameStroke").value)
+
+  viewAspect = parseInt(document.getElementById("viewAspect").value)
+  viewClass = parseInt(document.getElementById("viewClass").value)
+}
